5f3e5e14cc4868c5a3ab1d1235540bebda9ba0da
Ihor-Moroz <ihor.moroz.ukr@gmail.com>
2018-06-25 22:42:34 +0300

    two

--- src/Dates/Main.java
+++ src/Dates/Main.java
@@ -1,7 +1,21 @@
 package Dates;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HW");
+        Date date=new Date();
+        System.out.println(date);
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.YEAR,5);
+        DateFormat dateFormat=DateFormat.getDateInstance(DateFormat.MEDIUM);
+        System.out.println(dateFormat.format(calendar.getTime()));
+        SimpleDateFormat sdf=new SimpleDateFormat("dd.....MM......yyyy");//date and time patterns
+        System.out.println(sdf.format(calendar.getTime()));
     }
 }
