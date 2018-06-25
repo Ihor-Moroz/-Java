2117f6623b5d70a9fcc94241e8daa9bf29e45974
Ihor-Moroz <ihor.moroz.ukr@gmail.com>
2018-06-26 00:44:49 +0300

    bla bla bla

--- src/EqualsAndHashCode/Main.java
+++ src/EqualsAndHashCode/Main.java
@@ -1,7 +1,50 @@
 package EqualsAndHashCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
+class Book{
+    String author;
+    String name;
+}
+
+class Ticket{
+    int number;
+    String libraryName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ticket ticket = (Ticket) o;
+
+        if (number != ticket.number) return false;
+        return libraryName.equals(ticket.libraryName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = number;
+        result = 31 * result + libraryName.hashCode();
+        return result;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ghdcf");
+        Ticket ticket=new Ticket();
+        ticket.number=123;
+        Book book=new Book();
+        book.author="Lox";
+        book.name="Key";
+        Map<Ticket, Book> library = new HashMap<>();
+        library.put(ticket,book);
+
+        Ticket ticket2=new Ticket();
+        ticket2.number=123;
+
+        Book newBook=library.get(ticket2);
+        System.out.println(newBook.author);
+        System.out.println(newBook.name);
     }
 }
