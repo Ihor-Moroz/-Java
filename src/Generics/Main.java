559d4535842b81682ab51cca5b43af99e3365e99
Ihor-Moroz <ihor.moroz.ukr@gmail.com>
2018-06-28 23:23:58 +0300

    Generics

--- src/Generics/Main.java
+++ src/Generics/Main.java
@@ -1,11 +1,31 @@
 package Generics;
 
-import java.util.*;
+import java.io.Serializable;
 
-public class Main <T> {
+class Car extends Main implements Comparable, Serializable{
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
+
+
+public class Main <T extends Main & Comparable & Serializable> {
+
+    T var;
 
     public static void main(String[] args) throws Exception{
-       List<Main> list = new ArrayList<>();
-       List list2=new ArrayList();
+        Main<Car> main=new Main<>();
+        main.var=new Car();
+        System.out.println(main.method(main.var));
+    }
+
+    void someMethod(){
+        System.out.println("HW");
+    }
+    T method(T type){
+        type.someMethod();
+        return type;
     }
 }
