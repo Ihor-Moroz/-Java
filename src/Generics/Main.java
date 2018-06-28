4ba8501b96fd1443d14de8885fb5315203966239
Ihor-Moroz <ihor.moroz.ukr@gmail.com>
2018-06-28 23:15:40 +0300

    Generics

---
+++ src/Generics/Main.java
@@ -0,0 +1,11 @@
+package Generics;
+
+import java.util.*;
+
+public class Main <T> {
+
+    public static void main(String[] args) throws Exception{
+       List<Main> list = new ArrayList<>();
+       List list2=new ArrayList();
+    }
+}
